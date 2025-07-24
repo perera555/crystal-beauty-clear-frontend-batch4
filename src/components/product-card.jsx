d402148ffc6033e016b80eaea84376b9d691c454
Dhanushka <pthamara@rocketmail.com>
2025-07-24 21:06:19 +0530

    designing product Page✏️

--- src/components/product-card.jsx
+++ src/components/product-card.jsx
@@ -6,10 +6,18 @@ import { Link } from "react-router-dom";
 
 return(
     
-        <Link className="w-[250px] m-4 h-[350px] bg-amber-200  ">
-            <img className="w-full h-[220px]"  src={product.images[0]} />
+        <Link className="w-[250px] m-4 h-[350px]  shadow-2xl ">
+            <img className="w-full h-[220px] object-cover "  src={product.images[0]} />
+            <div className="h-[110px] w-full flex justify-center  flex-col px-4">
+                <p className="text-gray-400">{product.productId}</p>
+                <p className="text-lg font-bold">{product.name}</p>
+                <p className="text-lg text-pink-400">{product.price.toFixed(2)} <span className="line-through text-gray-400 text-sm">{product.price<product.labledPrice&&product.labledPrice.toFixed(2)}</span></p>
+
+
+
+            </div>
         </Link>
-        
+         
     
 )
 
